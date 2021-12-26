ae97e7671a41bf5347b066b0aec1c20a01bd6917
Nikita VASILEV <nvasilev@e3r13p13.clusters.42paris.fr>
2021-12-26 01:23:23 +0100

    mv to srcs

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,11 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 12:09:26 by nvasilev          #+#    #+#             */
-/*   Updated: 2021/10/19 15:35:32 by nvasilev         ###   ########.fr       */
+/*   Updated: 2021/12/26 00:47:29 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "includes/fdf.h"
+#include "../includes/fdf.h"
 
 int	deal_key(int key, t_params *params)
 {
@@ -31,7 +31,7 @@ int	main(void)
 	//memset(&params, 0, sizeof(t_params));
 	params.mlx_ptr = 0;
 	params.win_ptr = 0;
-	*params.tmp = 0;
+	params.tmp = 0;
 	params.mlx_ptr = mlx_init();
 	params.win_ptr = mlx_new_window(params.mlx_ptr, 500, 500, "MLX 42");
 	mlx_pixel_put(params.mlx_ptr, params.win_ptr, 250, 250, 0xFFFFFF);
