3be7d6024c36425d125ffc0ab8052b7fcf88d172
Nikita VASILEV <nvasilev@e3r13p13.clusters.42paris.fr>
2021-12-26 01:20:09 +0100

    updated utils protos

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,14 +6,15 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 14:49:23 by nvasilev          #+#    #+#             */
-/*   Updated: 2021/10/19 15:34:24 by nvasilev         ###   ########.fr       */
+/*   Updated: 2021/12/26 01:07:09 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
-# include <mlx.h>
+# include "../minilibx-linux/mlx.h"
+# include "get_next_line.h"
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
@@ -31,5 +32,6 @@ typedef struct s_params
 void	ft_bzero(void *s, size_t n);
 void	ft_putnbr_fd(int n, int fd);
 int		ft_putchar_fd(int c, int fd);
+void	ft_putstr_fd(const char *str, int fd);
 
 #endif
