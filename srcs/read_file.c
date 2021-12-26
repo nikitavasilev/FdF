5b193de19c1c0a8a49ebf626a31ba1e8ec213d2c
Nikita VASILEV <vassilev.nikita@gmail.com>
2021-12-26 08:53:02 +0100

    reading

---
+++ srcs/read_file.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   read_file.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/26 02:52:34 by nvasilev          #+#    #+#             */
+/*   Updated: 2021/12/26 08:52:38 by nvasilev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/fdf.h"
+
+int		ft_wdcounter(char const *str, char c)
+{
+	int i;
+	int words;
+
+	words = 0;
+	i = 0;
+	while (str[i])
+	{
+		while (str[i] == c && str[i] != '\0')
+			i++;
+		if (str[i])
+			words++;
+		while (str[i] != c && str[i] != '\0')
+			i++;
+	}
+	return (words);
+}
+
+int	get_height(const char *file_name)
+{
+	char	*line;
+	int		fd;
+	int		height;
+
+	fd = open(file_name, O_RDONLY, 0);
+	height = 0;
+	while ((line = get_next_line(fd)))
+	{
+		height++;
+		free(line);
+	}
+	close(fd);
+	return (height);
+}
+
+int	get_width(const char *file_name)
+{
+	char	*line;
+	int		fd;
+	int		width;
+
+	fd = open(file_name, O_RDONLY, 0);
+	line = get_next_line(fd);
+	width = ft_wdcounter(line, ' ');
+	free(line);
+	close(fd);
+	return (width);
+}
+
+void	fill_matrix(int *z_line, char *line)
+{
+	char	**nums;
+	int		i;
+
+	nums = ft_split(line, ' ');
+	i = 0;
+	while (nums[i])
+	{
+		z_line[i] = ft_atoi(nums[i]);
+		free(nums[i]);
+		i++;
+	}
+	free(nums);
+}
+
+void	read_map(const char *file_name, t_fdf *data)
+{
+	int		fd;
+	char	*line;
+	int		i;
+
+	data->height = get_height(file_name);
+	data->width = get_width(file_name);
+	data->z_matrix = (int **)malloc(sizeof(int *) * (data->height + 1));
+	i = 0;
+	while (i <= data->height)
+		data->z_matrix[i++] = (int *)malloc(sizeof(int) * (data->width + 1));
+	fd = open(file_name, O_RDONLY, 0);
+	i = 0;
+	while (i < data->height)
+	{
+		line = get_next_line(fd);
+		fill_matrix(data->z_matrix[i], line);
+		free(line);
+		i++;
+	}
+	close(fd);
+	data->z_matrix[i] = NULL;
+}
