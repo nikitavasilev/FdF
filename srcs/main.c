0a8ce099d8b1546632ba63444f78f6a1ac0fef05
Nikita VASILEV <vassilev.nikita@gmail.com>
2021-12-30 06:29:17 +0100

    del global functions

--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,13 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 12:09:26 by nvasilev          #+#    #+#             */
-/*   Updated: 2021/12/29 06:41:43 by nvasilev         ###   ########.fr       */
+/*   Updated: 2021/12/30 06:24:54 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/fdf.h"
+#include "fdf.h"
 
-void	free_and_quit(t_fdf *data)
+void	exit_success(t_fdf *data)
 {
 	int	i;
 
@@ -21,6 +21,7 @@ void	free_and_quit(t_fdf *data)
 		free(data->z_matrix[i++]);
 	free(data->z_matrix);
 	free(data->img);
+	free(data->mlx_ptr);
 	free(data);
 	exit(EXIT_SUCCESS);
 }
@@ -32,8 +33,7 @@ int	deal_key(int key, t_fdf *data)
 		mlx_destroy_image(data->mlx_ptr, data->img->img);
 		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
 		mlx_destroy_display(data->mlx_ptr);
-		free(data->mlx_ptr);
-		free_and_quit(data);
+		exit_success(data);
 	}
 	data->prev_key = key;
 	if (key == XK_Up)
@@ -53,21 +53,20 @@ int	main(int argc, char const *argv[])
 	t_fdf	*data;
 	t_data	*img;
 
-	(void)argc;
-	data = (t_fdf *)malloc(sizeof(t_fdf));
-	img = (t_data *)malloc(sizeof(t_data));
-	read_map(argv[1], data);
-	data->mlx_ptr = mlx_init();
-	data->win_ptr = mlx_new_window(data->mlx_ptr, WIDTH, HEIGHT, NAME);
-	img->img = mlx_new_image(data->mlx_ptr, WIDTH, HEIGHT);
-	img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel, &img->line_length,
-								&img->endian);
-	data->prev_key = 0;
-	data->shift_x = MENU_WIDTH + (WIDTH - (data->width * data->zoom)) / 2;
-	data->shift_y = (HEIGHT - (data->height * data->zoom)) / 2;
-	draw(data, img);
-	data->img = img;
-	mlx_key_hook(data->win_ptr, deal_key, data);
-	mlx_loop(data->mlx_ptr);
+	if (argc == 2)
+	{
+		img = (t_data *)malloc(sizeof(t_data));
+		if (!img)
+			err_alloc();
+		data = (t_fdf *)malloc(sizeof(t_fdf));
+		if (!data)
+			err_alloc();
+		init(data, img);
+		read_map(argv[1], data);
+		init_shift(data);
+		draw(data, img);
+		mlx_key_hook(data->win_ptr, deal_key, data);
+		mlx_loop(data->mlx_ptr);
+	}
 	return (0);
 }
