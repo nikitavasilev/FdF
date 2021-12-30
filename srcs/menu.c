c56e3303a5e09f7926a90e7620647ec9946a916e
Nikita VASILEV <vassilev.nikita@gmail.com>
2021-12-30 06:29:44 +0100

    use of macros instead of strs

--- srcs/menu.c
+++ srcs/menu.c
@@ -6,12 +6,13 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/28 00:53:07 by nvasilev          #+#    #+#             */
-/*   Updated: 2021/12/29 02:20:33 by nvasilev         ###   ########.fr       */
+/*   Updated: 2021/12/30 04:18:51 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/fdf.h"
-#include "../includes/color.h"
+#include "fdf.h"
+#include "color.h"
+#include "text.h"
 
 void	draw_line(float x, float y, float x1, float y1, t_fdf *data, t_data *img)
 {
@@ -34,7 +35,7 @@ void	draw_line(float x, float y, float x1, float y1, t_fdf *data, t_data *img)
 
 void	print_square(t_fdf *data, t_data *img)
 {
-	data->color = 0xFFFFFF;
+	data->color = COLOR_WHITE;
 	draw_line(10, 10, 200, 10, data, img);
 	draw_line(200, 10, 200, 240, data, img);
 	draw_line(10, 10, 10, 240, data, img);
@@ -52,12 +53,12 @@ void	print_menu(t_fdf *data, t_data *img)
 	win = data->win_ptr;
 	print_square(data, img);
 	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, img->img, 0, 0);
-	mlx_string_put(mlx, win, 40, y += 35, TEXT_COLOR, "----- How to Use -----");
-	mlx_string_put(mlx, win, 35, y += 35, TEXT_COLOR, "Zoom: Scroll or +/-");
-	mlx_string_put(mlx, win, 35, y += 30, TEXT_COLOR, "Move: Arrows");
-	mlx_string_put(mlx, win, 35, y += 30, TEXT_COLOR, "Rotate: Press & Move");
-	mlx_string_put(mlx, win, 35, y += 30, TEXT_COLOR, "Projection");
-	mlx_string_put(mlx, win, 55, y += 25, TEXT_COLOR, "ISO: Press [I]");
-	mlx_string_put(mlx, win, 55, y += 25, TEXT_COLOR, "Parallel: Press [P]");
-	mlx_string_put(mlx, win, WIDTH - 200, HEIGHT - 30, TEXT_COLOR, "Quit: Press [ESC] or [CTRL + C]");
+	mlx_string_put(mlx, win, 40, y += 35, COLOR_L_GREY, STR_MENU);
+	mlx_string_put(mlx, win, 35, y += 35, COLOR_L_GREY, STR_ZOOM);
+	mlx_string_put(mlx, win, 35, y += 30, COLOR_L_GREY, STR_MOVE);
+	mlx_string_put(mlx, win, 35, y += 30, COLOR_L_GREY, STR_ROTATE);
+	mlx_string_put(mlx, win, 35, y += 30, COLOR_L_GREY, STR_PROJECTION);
+	mlx_string_put(mlx, win, 55, y += 25, COLOR_L_GREY, STR_ISOMETRIC);
+	mlx_string_put(mlx, win, 55, y += 25, COLOR_L_GREY, STR_PARALLEL);
+	mlx_string_put(mlx, win, WIDTH - 200, HEIGHT - 30, COLOR_L_GREY, STR_QUIT);
 }
