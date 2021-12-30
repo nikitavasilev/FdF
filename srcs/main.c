66ffa024dc0e5396c57fca19f6b42fde8ae3157f
Nikita VASILEV <vassilev.nikita@gmail.com>
2021-12-30 06:42:11 +0100

    ++ exit_success()

--- srcs/main.c
+++ srcs/main.c
@@ -6,35 +6,16 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 12:09:26 by nvasilev          #+#    #+#             */
-/*   Updated: 2021/12/30 06:24:54 by nvasilev         ###   ########.fr       */
+/*   Updated: 2021/12/30 06:40:01 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	exit_success(t_fdf *data)
-{
-	int	i;
-
-	i = 0;
-	while (data->z_matrix[i])
-		free(data->z_matrix[i++]);
-	free(data->z_matrix);
-	free(data->img);
-	free(data->mlx_ptr);
-	free(data);
-	exit(EXIT_SUCCESS);
-}
-
 int	deal_key(int key, t_fdf *data)
 {
 	if ((key == 'c' && data->prev_key == XK_Control_L) || key == XK_Escape)
-	{
-		mlx_destroy_image(data->mlx_ptr, data->img->img);
-		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
-		mlx_destroy_display(data->mlx_ptr);
 		exit_success(data);
-	}
 	data->prev_key = key;
 	if (key == XK_Up)
 		data->shift_y -= 10;
