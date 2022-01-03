667ffd899a02a0781e362904fb608dfbc0563e11
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-01-03 08:53:16 +0100

    window close + init ctrls

---
+++ srcs/init_controls.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_controls.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/03 02:32:06 by nvasilev          #+#    #+#             */
+/*   Updated: 2022/01/03 02:32:59 by nvasilev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+static int	close_window(void *param)
+{
+	exit_success(param);
+	return (0);
+}
+
+void	init_controls(t_fdf *data)
+{
+	mlx_hook(data->win_ptr, 2, 1L<<0, key_press, data);
+	mlx_hook(data->win_ptr, 17, 1L<<5, close_window, data);
+	//mlx_hook(fdf->win, 4, 0, mouse_press, fdf);
+	//mlx_hook(fdf->win, 5, 0, mouse_release, fdf);
+	//mlx_hook(fdf->win, 6, 0, mouse_move, fdf);
+}
