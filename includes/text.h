bf6e9ba3102085d25d35281414d9a897b1dcf46c
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-01-03 08:54:25 +0100

    z-axis

--- includes/text.h
+++ includes/text.h
@@ -6,20 +6,23 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/30 04:05:35 by nvasilev          #+#    #+#             */
-/*   Updated: 2021/12/30 04:14:11 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/01/03 08:39:57 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TEXT_H
 # define TEXT_H
 
-# define STR_MENU		"----- How to Use -----"
+# define STR_MENU		"------ How to Use ------"
 # define STR_ZOOM		"Zoom: Scroll or +/-"
 # define STR_MOVE		"Move: Arrows"
 # define STR_ROTATE		"Rotate: Press & Move"
 # define STR_PROJECTION	"Projection"
 # define STR_ISOMETRIC	"ISO: Press [I]"
 # define STR_PARALLEL	"Parallel: Press [P]"
+# define STR_Z_AXIS		"Change Z-Axis"
+# define STR_Z_INC		"Inc: Press [Page Up]"
+# define STR_Z_DEC		"Dec: Press [Page Down]"
 # define STR_QUIT		"Quit: Press [ESC] or [CTRL + C]"
 
 #endif
