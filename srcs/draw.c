0241e3d7393d2351ebe2db3f43580f27973666b2
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-01-03 08:55:55 +0100

    bresenham cut

--- srcs/draw.c
+++ srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/26 06:31:38 by nvasilev          #+#    #+#             */
-/*   Updated: 2021/12/30 04:33:32 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/01/03 08:48:09 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,65 +18,69 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	char	*dst;
 
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+	*(unsigned int *)dst = color;
 }
 
-void	isometric(float *x, float *y, int z)
-{
-	int previous_x;
-	int previous_y;
-
-	previous_x = *x;
-	previous_y = *y;
-
-	*x = (previous_x - previous_y) * cos(0.523599);
-	*y = -z + (previous_x + previous_y) * sin(0.523599);
-}
-
-void	bresenham(float x, float y, float x1, float y1, t_fdf *data, t_data *img)
+void	draw_line(t_coord coord, t_fdf *data)
 {
 	float	x_step;
 	float	y_step;
 	int		max;
-	int		z;
-	int		z1;
 
-	z = data->z_matrix[(int)y][(int)x];
-	z1 = data->z_matrix[(int)y1][(int)x1];
-
-	//----------zoom---------
-	x *= data->zoom;
-	y *= data->zoom;
-	x1 *= data->zoom;
-	y1 *= data->zoom;
-
-	//----------color--------
-	data->color = (z || z1) ? 0xf7e436 : 0x40eb34;
-	//-----------3d----------
-	isometric(&x, &y, z);
-	isometric(&x1, &y1, z1);
-
-	//-----------shift--------
-	x += data->shift_x;
-	y += data->shift_y;
-	x1 += data->shift_x;
-	y1 += data->shift_y;
-
-	x_step = x1 - x;
-	y_step = y1 - y;
-
-	max = MAX(MOD(x_step), MOD(y_step));
+	x_step = coord.x1 - coord.x;
+	y_step = coord.y1 - coord.y;
+	max = (fmaxf(fabsf(x_step), fabsf(y_step)));
 	x_step /= max;
 	y_step /= max;
-	while ((int)(x - x1) || (int)(y - y1))
+	while ((int)(coord.x - coord.x1) || (int)(coord.y - coord.y1))
 	{
-		my_mlx_pixel_put(img, x, y, data->color);
-		x += x_step;
-		y += y_step;
+		my_mlx_pixel_put(data->img, coord.x, coord.y, data->color);
+		coord.x += x_step;
+		coord.y += y_step;
 	}
 }
 
-void	draw(t_fdf *data, t_data *img)
+void	isometric(float *x, float *y, int z)
+{
+	int	previous_x;
+	int	previous_y;
+
+	previous_x = *x;
+	previous_y = *y;
+	*x = (previous_x - previous_y) * cos(0.523599);
+	*y = -z + (previous_x + previous_y) * sin(0.523599);
+}
+
+void	bresenham(t_coord coord, t_fdf *data)
+{
+	if (coord.z || coord.z1)
+		data->color = 0xf7e436;
+	else
+		data->color = 0x40eb34;
+	if (data->is_iso)
+	{
+		isometric(&coord.x, &coord.y, coord.z);
+		isometric(&coord.x1, &coord.y1, coord.z1);
+	}
+	coord.x += data->shift_x;
+	coord.y += data->shift_y;
+	coord.x1 += data->shift_x;
+	coord.y1 += data->shift_y;
+	draw_line(coord, data);
+}
+
+t_coord	project(t_coord coord, t_fdf *data)
+{
+	coord.z = data->z_matrix[(int)coord.y][(int)coord.x];
+	coord.z1 = data->z_matrix[(int)coord.y1][(int)coord.x1];
+	coord.x *= data->zoom;
+	coord.y *= data->zoom;
+	coord.x1 *= data->zoom;
+	coord.y1 *= data->zoom;
+	return (coord);
+}
+
+void	draw_map(t_fdf *data, t_data *img)
 {
 	int	x;
 	int	y;
@@ -89,9 +93,9 @@ void	draw(t_fdf *data, t_data *img)
 		while (x < data->width)
 		{
 			if (x < data->width - 1)
-				bresenham(x, y, x + 1, y, data, img);
+				bresenham(project(get_coord(x, x + 1, y, y), data), data);
 			if (y < data-> height - 1)
-				bresenham(x, y, x, y + 1, data, img);
+				bresenham(project(get_coord(x, x, y, y + 1), data), data);
 			x++;
 		}
 		y++;
