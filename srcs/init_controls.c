2b367ab826d13e39b813499718969f4e20660b05
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-01-04 16:19:36 +0100

    del mouse control

--- srcs/init_controls.c
+++ srcs/init_controls.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 02:32:06 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/01/03 22:52:14 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/01/04 14:42:50 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,4 @@ void	init_controls(t_fdf *data)
 {
 	mlx_hook(data->win_ptr, 2, 1L << 0, key_press, data);
 	mlx_hook(data->win_ptr, 17, 1L << 5, close_window, data);
-	//mlx_hook(fdf->win, 4, 0, mouse_press, fdf);
-	//mlx_hook(fdf->win, 5, 0, mouse_release, fdf);
-	//mlx_hook(fdf->win, 6, 0, mouse_move, fdf);
 }
