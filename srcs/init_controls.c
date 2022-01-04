f15136266a4ae72bc3ce0e99dd7bfdd9879abdda
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-01-04 09:39:31 +0100

    norm

--- srcs/init_controls.c
+++ srcs/init_controls.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 02:32:06 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/01/03 02:32:59 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/01/03 22:52:14 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ static int	close_window(void *param)
 
 void	init_controls(t_fdf *data)
 {
-	mlx_hook(data->win_ptr, 2, 1L<<0, key_press, data);
-	mlx_hook(data->win_ptr, 17, 1L<<5, close_window, data);
+	mlx_hook(data->win_ptr, 2, 1L << 0, key_press, data);
+	mlx_hook(data->win_ptr, 17, 1L << 5, close_window, data);
 	//mlx_hook(fdf->win, 4, 0, mouse_press, fdf);
 	//mlx_hook(fdf->win, 5, 0, mouse_release, fdf);
 	//mlx_hook(fdf->win, 6, 0, mouse_move, fdf);
