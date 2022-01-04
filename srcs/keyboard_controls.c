f962a8b716a2538bff12cdd9b87b739e6a4e8176
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-01-04 09:39:06 +0100

    norm + move()

--- srcs/keyboard_controls.c
+++ srcs/keyboard_controls.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 02:12:30 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/01/03 08:25:33 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/01/03 22:48:51 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,33 +17,40 @@ void	change_z(int key, t_fdf *data)
 	int	i;
 	int	j;
 
-	i = 0;
-	while (i < data->height)
+	i = -1;
+	while (++i < data->height)
 	{
-		j = 0;
-		while (j < data->width)
+		j = -1;
+		while (++j < data->width)
 		{
-			if (data->z_matrix[i][j])
+			if (key == XK_Page_Up && data->z_matrix[i][j])
 			{
-				if (key == XK_Page_Up)
-				{
-					if (data->z_matrix[i][j] == -1)
-						data->z_matrix[i][j] += 2;
-					else
-						data->z_matrix[i][j]++;
-				}
-				else if (key == XK_Page_Down)
-				{
-					if (data->z_matrix[i][j] == 1)
-						data->z_matrix[i][j] -= 2;
-					else
-						data->z_matrix[i][j]--;
-				}
+				if (data->z_matrix[i][j] == -1)
+					data->z_matrix[i][j] += 2;
+				else
+					data->z_matrix[i][j]++;
 			}
-			j++;
+			else if (key == XK_Page_Down && data->z_matrix[i][j])
+			{
+				if (data->z_matrix[i][j] == 1)
+					data->z_matrix[i][j] -= 2;
+				else
+					data->z_matrix[i][j]--;
+			}
 		}
-		i++;
 	}
+}
+
+void	move(int key, t_fdf *data)
+{
+	if (key == XK_Up)
+		data->shift_y -= 10;
+	else if (key == XK_Down)
+		data->shift_y += 10;
+	else if (key == XK_Right)
+		data->shift_x += 10;
+	else if (key == XK_Left)
+		data->shift_x -= 10;
 }
 
 int	key_press(int key, t_fdf *data)
@@ -51,14 +58,8 @@ int	key_press(int key, t_fdf *data)
 	if ((key == 'c' && data->prev_key == XK_Control_L) || key == XK_Escape)
 		exit_success(data);
 	data->prev_key = key;
-	if (key == XK_Up)
-		data->shift_y -= 10;
-	if (key == XK_Down)
-		data->shift_y += 10;
-	if (key == XK_Right)
-		data->shift_x += 10;
-	if (key == XK_Left)
-		data->shift_x -= 10;
+	if (key == XK_Up || key == XK_Down || key == XK_Right || key == XK_Left)
+		move(key, data);
 	if (key == 'i')
 		data->is_iso = 1;
 	if (key == 'p')
